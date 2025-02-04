9fb13b70728f736064c3775041127769de594aad
merabza <merabza@gmail.com>
2025-02-04 22:42:35 +0400

    Initiak

---
+++ SupportToolsServerContracts/SupportToolsServerApiClient.cs
@@ -0,0 +1,16 @@
+﻿using ApiContracts;
+using Microsoft.Extensions.Logging;
+using StringMessagesApiContracts;
+
+namespace SupportToolsServerContracts;
+
+public class SupportToolsServerApiClient : ApiClient
+{
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public SupportToolsServerApiClient(ILogger logger, IHttpClientFactory httpClientFactory, string server, string? apiKey,
+        bool useConsole) : base(logger, httpClientFactory, server, apiKey, new StringMessageHubClient(server, apiKey),
+        useConsole)
+    {
+    }
+
+}
---
+++ SupportToolsServerContracts/V1/Routes/SupportToolsServerApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace SupportToolsServerContracts.V1.Routes;
+
+public static class SupportToolsServerApiRoutes
+{
+    private const string Root = "api";
+    private const string Version = "v1";
+    public const string ApiBase = Root + "/" + Version;
+
+
+    public static class Projects
+    {
+        public const string ProjectBase = "/projects";
+    }
+}
