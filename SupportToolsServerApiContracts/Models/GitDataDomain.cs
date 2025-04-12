981846acc2e64ed8e005156aaca3aacaf8cb2b89
merabza <merabza@gmail.com>
2025-04-12 23:49:52 +0400

    202504122349

--- SupportToolsServerApiContracts/Models/GitDataDomain.cs
+++ SupportToolsServerApiContracts/Models/GitDataDomain.cs
@@ -2,25 +2,8 @@
 
 public sealed class GitDataDomain
 {
-    //// ReSharper disable once ConvertToPrimaryConstructor
-    //public GitDataDomain(string gitProjectName, string gitProjectAddress, string gitProjectFolderName, string gitIgnorePathName)
-    //{
-    //    GitProjectName = gitProjectName;
-    //    GitProjectAddress = gitProjectAddress;
-    //    GitProjectFolderName = gitProjectFolderName;
-    //    GitIgnorePathName = gitIgnorePathName;
-    //}
-
-    //public GitDataDomain(string gitProjectAddress, string gitProjectFolderName, string gitIgnorePathName)
-    //{
-    //    GitProjectName = gitProjectFolderName;
-    //    GitProjectAddress = gitProjectAddress;
-    //    GitProjectFolderName = gitProjectFolderName;
-    //    GitIgnorePathName = gitIgnorePathName;
-    //}
-
-    public string GitProjectName { get; }
-    public string GitProjectAddress { get; set; }
-    public string GitProjectFolderName { get; set; }
-    public string GitIgnorePathName { get; }
+    public required string GitProjectName { get; set; }
+    public required string GitProjectAddress { get; set; }
+    public required string GitProjectFolderName { get; set; }
+    public required string GitIgnorePathName { get; set; }
 }
