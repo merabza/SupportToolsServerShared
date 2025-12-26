76da8705fb95fec23e30e1cafeb089900decfbc2
merabza <merabza@gmail.com>
2025-12-26 08:40:21 +0400

    202512260839

--- SupportToolsServerApiContracts/Models/StsGitIgnoreFileTypeDataModel.cs
+++ SupportToolsServerApiContracts/Models/StsGitIgnoreFileTypeDataModel.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace SupportToolsServerApiContracts.Models;
 
 public sealed class StsGitIgnoreFileTypeDataModel
 {
+    public Guid RowId { get; set; }
     public required string Name { get; set; }
     public required string Content { get; set; }
 }
