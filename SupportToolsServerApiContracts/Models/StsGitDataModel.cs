aa711dc7cc103f9fe06c8a167de87b4ca63847f5
merabza <merabza@gmail.com>
2026-09-19 17:32:52 +0400

    202609191732

--- SupportToolsServerApiContracts/Models/StsGitDataModel.cs
+++ SupportToolsServerApiContracts/Models/StsGitDataModel.cs
@@ -5,5 +5,5 @@ public sealed class StsGitDataModel
     public required string GitProjectName { get; set; }
     public required string GitProjectAddress { get; set; }
     public required string GitProjectFolderName { get; set; }
-    public required string GitIgnorePathName { get; set; }
+    public required string GitIgnorePatternName { get; set; }
 }
